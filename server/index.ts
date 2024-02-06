28fb911bf7e4470a586fe0fa6b4d5d5a3b17e98f
theziko1 <zakariaghachim96@gmail.com>
2024-02-06 21:39:37 +0100

    create server and connection of database

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,29 @@
-console.log("test test")
+import express , { Express, Request, Response  }  from "express";
+import { config } from "dotenv";
+import { connect } from "mongoose";
+import morgan from "morgan"
+
+
+// config
+config()
+// init express app
+const app : Express = express()
+// init PORT
+const PORT = process.env.PORT
+// middlewares
+app.use(morgan("combined"))
+app.use(express.json())
+// Routes 
+
+// connect of database
+connect(process.env.MONGO_URL as string)
+try {
+    console.log("connected to the database")
+} catch (error) {
+    console.log("connexion failed",error) 
+}
+
+// listen of app 
+app.listen(PORT,()=>{
+    console.log(`server running on PORT ${PORT}`)
+})
