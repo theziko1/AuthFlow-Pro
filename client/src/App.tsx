8cc6748640efb6f3c7c7325a1c8920487bc05cb4
theziko1 <zakariaghachim96@gmail.com>
2024-02-09 23:05:28 +0100

    create sign in page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes , Route} from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
 function App() {
  
@@ -9,6 +10,7 @@ function App() {
     <>
      <Routes>
          <Route path="/" element={<Signup/>}/>
+         <Route path="/sign-in" element={<Signin/>}/>
      </Routes>
     </>
   )
--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border-b-2 border-black outline-none bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "w-full border-b-2 focus:border-orange-500 outline-none placeholder:text-gray-900",
           className
         )}
         ref={ref}
