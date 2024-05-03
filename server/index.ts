993ff3be5837174503fe4dc0f1d9777515e56524
Zakaria Ghachim <98032256+theziko1@users.noreply.github.com>
2024-05-03 16:26:43 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials : true,
-    origin : "https://authflow-pro.onrender.com/",
+    origin : "*",
 }))
 // Routes 
 app.use("/",UserRoutes)
